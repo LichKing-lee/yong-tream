d05ac8cc62c326b4613c1904244f2decf78608df
LeeChangYong <lcy9002@naver.com>
2016-10-09 16:14:37 +0900

    stream 골격 구현체 생성, map, filter 구현, QUnit 테스트 환경 설정

--- js/yong-tream.js
+++ js/yong-tream.js
@@ -2,3 +2,34 @@
  * Created by ChangYong on 2016. 10. 9..
  * version 1.0.0
  */
+(function(){
+    var Stream = function(arr){
+        this._arr = arr;
+    };
+
+    var fn = Stream.prototype;
+
+    fn.get = function(){
+        return this._arr;
+    };
+
+    fn.map = function(func){
+        var arr = [];
+        this._arr.forEach(function(val){
+            arr.push(func(val));
+        });
+        return new Stream(arr);
+    };
+
+    fn.filter = function(predicate){
+        var arr = [];
+        this._arr.forEach(function(val){
+            if(predicate(val) === true){
+                arr.push(val);
+            }
+        });
+        return new Stream(arr);
+    };
+
+    window.Yong = Stream;
+})();
